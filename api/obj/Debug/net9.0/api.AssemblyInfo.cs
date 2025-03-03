4b9bf3c86b7ce37cca807aefa8ec67fc3e50dabf
MissKluck <sigrid.b.kluck@gmail.com>
2025-03-03 14:54:39 +0100

    moved gitignore

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52427ce5d422ae634e50b3c4daf14c77c5301dc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b134a21e4705052ea7965b9efabbdfb9f4fef46")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
