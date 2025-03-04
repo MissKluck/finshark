731556cb09c179769e8a055d41197e3e9fef71c6
MissKluck <sigrid.b.kluck@gmail.com>
2025-03-04 15:16:39 +0100

    setup comment model and connected it to stock model

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -7,6 +7,15 @@ namespace api.Models
 {
     public class Comment
     {
-        
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        //connect the comments to the stock model
+        public int? StockId { get; set; }
+        //Navigation property -> allows us to navigate within our models
+        public Stock? Stock { get; set; }
+
     }
 }
