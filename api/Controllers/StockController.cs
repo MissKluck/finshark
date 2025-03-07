e4c77c39c8beac60f59b7e531e2849bdde53ed27
MissKluck <sigrid.b.kluck@gmail.com>
2025-03-07 11:21:44 +0100

    Finished setup for stockcontroller

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -11,13 +11,33 @@ namespace api.Controllers
 {
     [Route("api/stock")]
     [ApiController]
-    public class StockController(ApplicationDbContext context) : ControllerBase
+    public class StockController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList();
+            
             return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(stock);
         }
     }
 }
